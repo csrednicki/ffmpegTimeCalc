6a60719306a9b177a61d337423f6023cfb9b1165
Cezary Srednicki <csrednicki@gmail.com>
2017-08-13 01:13:11 +0200

    Added multiline support
    Expected multiline format is:
    
    episodename
    timestamp start
    timestamp end
    
    episodename
    timestamp start
    timestamp end
    
    etc...

--- js/main.js
+++ js/main.js
@@ -7,10 +7,37 @@
     }
 
     function bindEvents() {
+        bindGenerateBtn();
+        bindNavClick();
+    }
+
+    function bindNavClick() {
+        let navSingleBtn = document.getElementById("navSingle");
+        let navMultiBtn = document.getElementById("navMulti");
+
+        navSingleBtn.addEventListener("click", function (event) {
+            event.preventDefault();
+            document.getElementById("mainForm").className = "form-horizontal single";
+        }, false);
+
+        navMultiBtn.addEventListener("click", function (event) {
+            event.preventDefault();
+            document.getElementById("mainForm").className = "form-horizontal multiline";
+        }, false);
+    }
+
+    function bindGenerateBtn() {
         let mainButton = document.getElementById("generate");
         mainButton.addEventListener("click", function (event) {
             event.preventDefault();
-            renderCommands();
+
+            if (document.querySelector('form.multiline') !== null) {
+                // multiline
+                renderMultiline();
+            } else {
+                // single
+                renderSingle();
+            }
         }, false);
     }
 
@@ -37,28 +64,21 @@
         return formatted;
     }
 
-    function calculateDuration() {
-        let start = document.getElementById('startTime').value;
-        let end = document.getElementById('endTime').value;
-
+    function calculateDuration(start, end) {
         let startSec = moment.duration(start);
         let calculated = moment.duration(end).subtract(startSec);
 
         return formatMomentTime(calculated);
     }
 
-    function calculateStartOffset() {
-        let start = document.getElementById('startTime').value;
-
+    function calculateStartOffset(start) {
         let offset = moment.duration(timeOffset, 'seconds');
         let calculated = moment.duration(start).subtract(offset);
 
         return formatMomentTime(calculated);
     }
 
-    function calculateEndOffset() {
-        let end = document.getElementById('endTime').value;
-
+    function calculateEndOffset(end) {
         let dt = moment.duration(durationTest, 'seconds');
         let calculated = moment.duration(end).subtract(dt);
 
@@ -76,38 +96,67 @@
         return formatMomentTime(dt);
     }
 
-    function formatOutputFilename(suffix) {
-        let filename = document.getElementById('outputFilename').value;
-        let filenameParts = filename.split('.');
-
-        if (suffix) {
-            filename = `${filenameParts[0]}_${suffix}.${filenameParts[1]}`;
-        }
-
-        return filename;
+    function formatOutputFilename(filename) {
+        return filename+'.mp4';
     }
 
-    function renderCommands() {
-        let startOffset = calculateStartOffset();
+    function renderSingle() {
+        let startOffset = calculateStartOffset( document.getElementById('startTime').value );
         let specificOffset = calculateSpecificOffset();
         let durationTest = calculateDurationTest();
         let filters = getFilters();
-        let endOffset = calculateEndOffset();
-        let duration = calculateDuration();
+        let endOffset = calculateEndOffset( document.getElementById('endTime').value );
+        let duration = calculateDuration( document.getElementById('startTime').value, document.getElementById('endTime').value );
 
         // filenames
         let inputFilename = document.getElementById('inputFilename').value;
-        let outputFilename = formatOutputFilename();
+        let outputFilename = formatOutputFilename( document.getElementById('outputFilename').value );
         let outputFilenameStartTest = formatOutputFilename('start');
         let outputFilenameEndTest = formatOutputFilename('end');
 
-        let cmdStartTest = `ffmpeg -ss ${startOffset} -i ${inputFilename} -ss ${specificOffset} -t ${durationTest} -c:a copy -c:v libx264 ${filters} -preset fast -crf 25 -y ${outputFilenameStartTest}`;
-        let cmdEndTest = `ffmpeg -ss ${endOffset} -i ${inputFilename} -t ${durationTest} -c:a copy -c:v libx264 ${filters} -preset fast -crf 25 -y ${outputFilenameEndTest}`;
-        let cmd = `ffmpeg -ss ${startOffset} -i ${inputFilename} -ss ${specificOffset} -t ${duration} -c:a copy -c:v libx264 ${filters} -preset slow -crf 25 ${outputFilename}`;
+        let cmdStartTest = `ffmpeg -ss ${startOffset} -i "${inputFilename}" -ss ${specificOffset} -t ${durationTest} -c:a copy -c:v libx264 ${filters} -preset fast -crf 25 -y "${outputFilenameStartTest}"`;
+        let cmdEndTest = `ffmpeg -ss ${endOffset} -i "${inputFilename}" -t ${durationTest} -c:a copy -c:v libx264 ${filters} -preset fast -crf 25 -y "${outputFilenameEndTest}"`;
+        let cmd = `ffmpeg -ss ${startOffset} -i "${inputFilename}" -ss ${specificOffset} -t ${duration} -c:a copy -c:v libx264 ${filters} -preset slow -crf 25 "${outputFilename}"`;
 
         document.getElementById('cmdStartTest').value = cmdStartTest;
         document.getElementById('cmdEndTest').value = cmdEndTest;
         document.getElementById('cmd').value = cmd;
+    }
+
+    function renderMultiline() {
+        // clean up output textarea
+        document.getElementById('cmd').value = '';
+
+        let times = document.getElementById('times').value;
+        let lines = times.split("\n\n");
+
+        lines.forEach(function(element) {
+            console.log(element);
+
+            generateEpisode(element);
+        }, this);
+    }
+
+    function generateEpisode(episode) {
+        let lines = episode.split("\n");
+
+        let filename = lines[0];
+        let startTime = lines[1];
+        let endTime = lines[2];
+
+        if(filename !== '' && startTime !== '' && endTime !== '') {
+
+            let startOffset = calculateStartOffset( startTime );
+            let specificOffset = calculateSpecificOffset();
+            let filters = getFilters();
+            let duration = calculateDuration( startTime, endTime );
+            let inputFilename = document.getElementById('inputFilename').value;
+            let outputFilename = formatOutputFilename( filename );
+
+            let cmd = `ffmpeg -ss ${startOffset} -i "${inputFilename}" -ss ${specificOffset} -t ${duration} -c:a copy -c:v libx264 ${filters} -preset slow -crf 25 "${outputFilename}"`;
+
+            document.getElementById('cmd').value += cmd + "\n";        
+        }
     }
 
     init();
